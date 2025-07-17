5f0910a82c1ebb15436d7c054cbd83704c516e0b
dstnamefish <1651862368@qq.com>
2025-07-17 20:53:27 +0800

    refactor: simplify app structure by replacing main layout with Provider and RouterView components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <div id="app">
-    <!-- 路由出口 -->
-    <main class="container mx-auto px-4 py-8 min-h-screen">
-      <router-view></router-view>
-    </main>
-  </div>
+  <Provider>
+    <RouterView/>
+  </Provider>
 </template>
+
 <script setup lang="ts">
-
+import { RouterView } from 'vue-router';
 </script>
-
 <style scoped>
-
+@import './assets/styles/global.scss';
 </style>
