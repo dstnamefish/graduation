ac918ffd58acd31520c49e1695d819018af21599
dstnamefish <1651862368@qq.com>
2025-07-17 20:41:19 +0800

    feat: 添加多个视图组件，包括 DepartmentView、DoctorView、ExpenseView、MedicineView、PatientView、QueryView、RegisterView、ReservationView，均包含计数器功能

---
+++ src/views/DepartmentView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="counter-container">
+    <button @click="counter++">Increment</button>
+    <p>Count: {{ counter }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const counter = ref(0);
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home-view">
+    <div class="home-header">
+      <div class="home-header-left">
+        <WelcomeBanner />
+        <DashboardCards />
+      </div>
+      <div class="home-header-right">
+        <CalendarFilter />
+      </div>
+    </div>
+    <div class="home-content">
+      <HospitalTrend />
+      <AppointmentList />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppointmentList from '../components/business/home/AppointmentList.vue';
+import CalendarFilter from '../components/business/home/CalendarFilter.vue';
+import DashboardCards from '../components/business/home/DashboardCards.vue';
+import HospitalTrend from '../components/business/home/HospitalTrend.vue';
+import WelcomeBanner from '../components/business/home/WelcomeBanner.vue';
+</script>
+
+<style scoped lang="scss">
+.home-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+
+  &-left {
+    display: flex;
+    flex-direction: column;
+    gap: 2.1rem;
+  }
+}
+
+.home-content {
+  display: flex;
+  gap: 2.1rem;
+}
+</style>
